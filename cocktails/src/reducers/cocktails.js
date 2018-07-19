7ad0ff670ed4a44b9a3094645c2b1e987975f1e1
Hajime2222 <hajime.doublebass@gmail.com>
2018-07-19 23:21:24 +0200

    add condition within switch statement

--- cocktails/src/reducers/cocktails.js
+++ cocktails/src/reducers/cocktails.js
@@ -2,6 +2,11 @@ import initialState from '../data/cocktailData'
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
+    case 'SELECT_COCK':
+      console.log('select coctail.')
+      console.log('id: ', action.payload)
+      // leave state unchange for now.
+      return state
     default:
       return state
   }
